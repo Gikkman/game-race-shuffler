196a94499ee4de3bb53d2336e88f870bf065b810
gikkman <gikk@gikkman.com>
2024-05-20 23:07:58 +0200

    Compile tweeks

--- compile.sh
+++ compile.sh
@@ -1,5 +1,18 @@
-#! /usr/local/env bash
-mkdir -p dist
+#! /usr/bin/env bash
+rm -rf ./dist
+mkdir -p ./dist
+
 npm run build
-docker run --rm -it -v $PWD:/home/bun/app oven/bun:1.1.8 build ./packages/client/src/index.ts --compile --outfile ./dist/grs-win --target=bun-windows-x64 "$1" "$2"
+docker run --rm -it -v $PWD:/home/bun/app oven/bun:1.1.8 build ./packages/client/src/index.ts --compile --outfile ./dist/grs-win --target=bun-windows-x64
+
 cp -r ./packages/client/lua ./dist
+cp ./client-config-A.json ./dist/client-config.json
+cat > ./dist/RUN.bat <<EOF
+@echo off
+
+set LOG_LEVEL=DEBUG
+start /WAIT /B ./grs-win.exe
+
+echo Press enter to exit...
+set /p input=
+EOF
