37a82ca562b504280bc03b99f2adbcfc132d2dbc
gikkman <gikk@gikkman.com>
2024-05-20 22:53:48 +0200

    Update compile

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #! /usr/local/env bash
 mkdir -p dist
 npm run build
-docker run --rm -it -v $PWD:/home/bun/app oven/bun:1.1.8 build ./packages/client/src/index.ts --compile --outfile ./dist/grs-win --target=bun-windows-x64
+docker run --rm -it -v $PWD:/home/bun/app oven/bun:1.1.8 build ./packages/client/src/index.ts --compile --outfile ./dist/grs-win --target=bun-windows-x64 "$1" "$2"
 cp -r ./packages/client/lua ./dist
